ed694759cf303b346aec0aa872919aa1d8c23c44
Yota Toyama <raviqqe@gmail.com>
2022-07-05 04:41:12 +0000

    Refactor

--- build.rs
+++ build.rs
@@ -7,28 +7,56 @@
 extern crate bindgen;
 
 use std::env;
+use std::error::Error;
 use std::fs;
-use std::path::PathBuf;
+use std::io;
+use std::path::Path;
 use std::process::Command;
+use std::str;
 
-fn llvm_config_shim(arg: &str) -> String {
-    let call = format!("llvm-config {}", arg);
-    let tg = if cfg!(target_os = "windows") {
-        Command::new("cmd")
-            .args(["/C", &call[..]])
-            .output()
-            .expect("failed to execute process")
-    } else {
-        Command::new("sh")
-            .arg("-c")
-            .arg(&call[..])
-            .output()
-            .expect("failed to execute process")
+fn main() {
+    run().unwrap()
+}
+
+fn run() -> Result<(), Box<dyn Error>> {
+    println!("cargo:rerun-if-changed=wrapper.h");
+    println!("cargo:rerun-if-changed=build.rs");
+
+    println!("cargo:rustc-link-search=all={}", llvm_config("--libdir")?);
+
+    if let Some(name) = get_system_libcpp() {
+        println!("cargo:rustc-link-lib=dylib={}", name);
     }
-    .stdout;
-    let mut s = String::from_utf8_lossy(&tg);
-    s.to_mut().pop();
-    return s.to_string();
+
+    for name in fs::read_dir(llvm_config("--libdir")?)?
+        .map(|entry| {
+            Ok(entry?
+                .path()
+                .file_name()
+                .and_then(|name| name.to_str())
+                .map(String::from))
+        })
+        .collect::<Result<Vec<_>, io::Error>>()?
+        .into_iter()
+        .flatten()
+    {
+        if (name.contains("libLLVM") || name.contains("libMLIR")) && name.ends_with(".a") {
+            println!(
+                "cargo:rustc-link-lib={}",
+                name.trim_start_matches("lib").trim_end_matches(".a")
+            );
+        }
+    }
+
+    bindgen::builder()
+        .header("wrapper.h")
+        .clang_arg(format!("-I{}", llvm_config("--includedir")?))
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .unwrap()
+        .write_to_file(Path::new(&env::var("OUT_DIR")?).join("bindings.rs"))?;
+
+    Ok(())
 }
 
 fn get_system_libcpp() -> Option<&'static str> {
@@ -43,46 +71,17 @@ fn get_system_libcpp() -> Option<&'static str> {
     }
 }
 
-fn llvm_libs() -> Vec<String> {
-    let libdir = llvm_config_shim("--libdir");
-    let paths = fs::read_dir(libdir).unwrap();
-    let names = paths
-        .filter_map(|entry| {
-            entry.ok().and_then(|e| {
-                e.path()
-                    .file_name()
-                    .and_then(|n| n.to_str().map(|s| String::from(s)))
-            })
-        })
-        .collect::<Vec<String>>();
-    return names;
-}
+fn llvm_config(argument: &str) -> Result<String, Box<dyn Error>> {
+    let call = format!("llvm-config {}", argument);
 
-fn main() {
-    println!("cargo:rerun-if-changed=wrapper.h");
-    println!("cargo:rerun-if-changed=build.rs");
-    let includedir = llvm_config_shim("--includedir");
-    let libdir = llvm_config_shim("--libdir");
-    println!("cargo:libdir={}", libdir);
-    println!("cargo:rustc-link-search=all={}", libdir);
-    println!(
-        "cargo:rustc-link-lib=dylib={}",
-        get_system_libcpp().unwrap()
-    );
-    for l in llvm_libs().iter() {
-        if (l.contains("libLLVM") || l.contains("libMLIR")) && l.ends_with(".a") {
-            let len = l.len();
-            println!("cargo:rustc-link-lib={}", l[3..(len - 2)].to_string());
+    Ok(str::from_utf8(
+        &if cfg!(target_os = "windows") {
+            Command::new("cmd").args(["/C", &call]).output()?
+        } else {
+            Command::new("sh").arg("-c").arg(&call).output()?
         }
-    }
-    let bindings = bindgen::builder()
-        .header("wrapper.h")
-        .clang_arg(format!("-I/{}", includedir))
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .generate()
-        .expect("Unable to generate bindings.");
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+        .stdout,
+    )?
+    .trim()
+    .to_string())
 }
