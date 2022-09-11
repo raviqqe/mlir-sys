cb25f5d2e218e55f4527e56cc609f5dd4dfa0e53
Yota Toyama <raviqqe@gmail.com>
2022-09-11 20:31:20 +0000

    Refactor

--- tools/setup.sh
+++ tools/setup.sh
@@ -8,10 +8,8 @@ set -e
 # brew update
 # brew install llvm@$llvm_version
 
+# echo PATH=$(brew --prefix)/opt/llvm@$llvm_version/bin:$PATH >>$GITHUB_ENV
+
 curl -fsSL https://apt.llvm.org/llvm.sh | sudo bash -s 15 all
 sudo apt -y install libmlir-15-dev mlir-15-tools
 echo PATH=/usr/lib/llvm-15/bin:$PATH >>$GITHUB_ENV
-
-if [ -n "$GITHUB_ENV" ]; then
-  echo PATH=$(brew --prefix)/opt/llvm@$llvm_version/bin:$PATH >>$GITHUB_ENV
-fi
