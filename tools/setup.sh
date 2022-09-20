4359f7d206f5e1a728e7e210d5a46778f667fdd8
Yota Toyama <raviqqe@gmail.com>
2022-09-19 21:53:16 -0700

    Fix

--- tools/setup.sh
+++ tools/setup.sh
@@ -10,5 +10,5 @@ brew install llvm@$llvm_version
 echo MLIR_SYS_150_PREFIX=$(brew --prefix)/opt/llvm@$llvm_version >>$GITHUB_ENV
 
 if [ $(uname) = Linux ]; then
-  sudo apt -y install libz3 libzstd1
+  sudo apt -y install libz3-dev libzstd-dev
 fi
