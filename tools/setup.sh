000db6963831f3f797c43e91eade4d86367d6757
Yota Toyama <raviqqe@gmail.com>
2022-09-20 05:41:56 +0000

    Revert "Set LD_LIBRARY_PATH"
    This reverts commit b3d7d1e3eefbcdbd2656fc10b47e1c29e9e93007.

--- tools/setup.sh
+++ tools/setup.sh
@@ -8,4 +8,3 @@ brew update
 brew install llvm@$llvm_version
 
 echo MLIR_SYS_150_PREFIX=$(brew --prefix)/opt/llvm@$llvm_version >>$GITHUB_ENV
-echo LD_LIBRARY_PATH=$(brew --prefix)/lib >>$GITHUB_ENV
