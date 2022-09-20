f917bc8efb2b8bed4a1fac4e60812733a57717c7
Yota Toyama <raviqqe@gmail.com>
2022-09-19 21:10:48 -0700

    Revert "Fix"
    This reverts commit d5628a8c0f65587cfba27cd96aba692bc5175efe.

--- tools/setup.sh
+++ tools/setup.sh
@@ -5,6 +5,6 @@ set -e
 llvm_version=15
 
 brew update
-brew install llvm@$llvm_version z3 zstd
+brew install llvm@$llvm_version
 
 echo MLIR_SYS_150_PREFIX=$(brew --prefix)/opt/llvm@$llvm_version >>$GITHUB_ENV
