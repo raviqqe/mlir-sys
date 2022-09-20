b3d7d1e3eefbcdbd2656fc10b47e1c29e9e93007
Yota Toyama <raviqqe@gmail.com>
2022-09-19 22:21:15 -0700

    Set LD_LIBRARY_PATH

--- tools/setup.sh
+++ tools/setup.sh
@@ -8,3 +8,4 @@ brew update
 brew install llvm@$llvm_version
 
 echo MLIR_SYS_150_PREFIX=$(brew --prefix)/opt/llvm@$llvm_version >>$GITHUB_ENV
+echo LD_LIBRARY_PATH=$(brew --prefix)/lib >>$GITHUB_ENV
