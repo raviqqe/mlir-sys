ffca5955f18acc9ee303107d9b8d19695e5c992f
Yota Toyama <raviqqe@gmail.com>
2022-09-20 04:45:44 +0000

    Install via apt

--- tools/setup.sh
+++ tools/setup.sh
@@ -8,3 +8,7 @@ brew update
 brew install llvm@$llvm_version
 
 echo MLIR_SYS_150_PREFIX=$(brew --prefix)/opt/llvm@$llvm_version >>$GITHUB_ENV
+
+if [ $(uname) = Linux ]; then
+  sudo apt -y install libz3 libzstd1
+fi
