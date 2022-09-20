665a34b371c0b8390a48a8d89f50c15c05c947c1
Yota Toyama <raviqqe@gmail.com>
2022-09-19 21:59:23 -0700

    Install pkg-config

--- tools/setup.sh
+++ tools/setup.sh
@@ -5,6 +5,6 @@ set -e
 llvm_version=15
 
 brew update
-brew install llvm@$llvm_version
+brew install llvm@$llvm_version pkg-config
 
 echo MLIR_SYS_150_PREFIX=$(brew --prefix)/opt/llvm@$llvm_version >>$GITHUB_ENV
