eff6162ea3a9c0a4f69e046ca64879e291df2c4f
Yota Toyama <raviqqe@gmail.com>
2022-09-19 22:20:04 -0700

    Revert "TEST"
    This reverts commit a23ae280b52c71bbd278519895efdf2f88137ba0.

--- tools/setup.sh
+++ tools/setup.sh
@@ -8,7 +8,3 @@ brew update
 brew install llvm@$llvm_version
 
 echo MLIR_SYS_150_PREFIX=$(brew --prefix)/opt/llvm@$llvm_version >>$GITHUB_ENV
-
-ls -l $(brew --prefix)/lib
-file /home/linuxbrew/.linuxbrew/lib/libz3.so
-file /home/linuxbrew/.linuxbrew/lib/libzstd.so.1.5.2
