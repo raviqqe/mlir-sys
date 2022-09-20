a23ae280b52c71bbd278519895efdf2f88137ba0
Yota Toyama <raviqqe@gmail.com>
2022-09-19 22:11:37 -0700

    TEST

--- tools/setup.sh
+++ tools/setup.sh
@@ -8,3 +8,7 @@ brew update
 brew install llvm@$llvm_version pkg-config
 
 echo MLIR_SYS_150_PREFIX=$(brew --prefix)/opt/llvm@$llvm_version >>$GITHUB_ENV
+
+ls -l $(brew --prefix)/lib
+file /home/linuxbrew/.linuxbrew/lib/libz3.so
+file /home/linuxbrew/.linuxbrew/lib/libzstd.so.1.5.2
